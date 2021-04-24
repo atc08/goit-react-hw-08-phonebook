d055fcd70de442c9ea48ed857723e1fa9b3fe496
Yevhenii Korkots <atc0884@gmail.com>
2021-04-25 01:12:13 +0300

    minor fixes

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getUserName, logOut } from '../../redux/auth';
-// import defaultAvatar from './default-avatar.png';
 import s from './UserMenu.module.scss';
 
-const UserMenu = ({ /*avatar,*/ name, onLogout }) => (
+const UserMenu = ({ name, onLogout }) => (
   <div className={s.container}>
-    {/* <img src={avatar} alt="" width="32" className={s.avatar} /> */}
     <span className={s.name}>Welcome, {name}</span>
     <button type="button" onClick={onLogout}>
       LogOut
@@ -16,7 +14,6 @@ const UserMenu = ({ /*avatar,*/ name, onLogout }) => (
 
 const mapStateToProps = state => ({
   name: getUserName(state),
-  // avatar: defaultAvatar,
 });
 
 const mapDispatchToProps = {
